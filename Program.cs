3e4fb5eda0bf3c261bb99c8d4b913abbd4b4f930
Sebastian Chaves <sebastian.chaves1408@gmail.com>
2023-06-30 22:39:11 -0600

    Operaciones matemáticas y lógicas en C#

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+class Program
+{
+    static void Main (string[] args)
+    {
+        Console.WriteLine ("Operaciones y lógica variable Byte");
+        byte a = 50;
+        byte b = 25;
+        Console.WriteLine (a + b);
+        Console.WriteLine (a - b);
+        Console.WriteLine (a * b);
+        Console.WriteLine (a / b);
+        Console.WriteLine (a % b);
+        Console.WriteLine (a > b);
+        Console.WriteLine (a < b);
+        Console.WriteLine (a == b);
+        Console.WriteLine (a >= b);
+        Console.WriteLine (a <= b);
+        Console.WriteLine (a != b);
+
+        Console.WriteLine ("Operaciones y lógica variable Short");
+        short e = -5;
+        short f = 10;
+        Console.WriteLine (e + f);
+        Console.WriteLine (e - f);
+        Console.WriteLine (e * f);
+        Console.WriteLine (e / f);
+        Console.WriteLine (e % f);
+        Console.WriteLine (e > f);
+        Console.WriteLine (e < f);
+        Console.WriteLine (e == f);
+        Console.WriteLine (e >= f);
+        Console.WriteLine (e <= f);
+        Console.WriteLine (e != f);
+
+        Console.WriteLine ("Operaciones y lógica variable Integer");
+        int g = 2023;
+        int h = 5000;
+        Console.WriteLine (g + h);
+        Console.WriteLine (g - h);
+        Console.WriteLine (g * h);
+        Console.WriteLine (g / h);
+        Console.WriteLine (g % h);
+        Console.WriteLine (g > h);
+        Console.WriteLine (g < h);
+        Console.WriteLine (g == h);
+        Console.WriteLine (g >= h);
+        Console.WriteLine (g <= h);
+        Console.WriteLine (g != h);
+
+        Console.WriteLine ("Operaciones y lógica variable Float");
+        float i = 0.55555F;
+        float j = -0.45689F;
+        Console.WriteLine (i + j);
+        Console.WriteLine (i - j);
+        Console.WriteLine (i * j);
+        Console.WriteLine (i / j);
+        Console.WriteLine (i % j);
+        Console.WriteLine (i > j);
+        Console.WriteLine (i < j);
+        Console.WriteLine (i == j);
+        Console.WriteLine (i >= j);
+        Console.WriteLine (i <= j);
+        Console.WriteLine (i != j);
+
+        Console.WriteLine ("Operaciones y lógica variable Double");
+        double k = 9999999999.99D;
+        double l = 1234567.89D;
+        Console.WriteLine (k + l);
+        Console.WriteLine (k - l);
+        Console.WriteLine (k * l);
+        Console.WriteLine (k / l);
+        Console.WriteLine (k % l);
+        Console.WriteLine (k > l);
+        Console.WriteLine (k < l);
+        Console.WriteLine (k == l);
+        Console.WriteLine (k >= l);
+        Console.WriteLine (k <= l);
+        Console.WriteLine (k != l);
+
+        Console.WriteLine ("Operaciones y lógica variable Long");
+        long m = 123456789;
+        long n = 123456789;
+        Console.WriteLine (m + n);
+        Console.WriteLine (m - n);
+        Console.WriteLine (m * n);
+        Console.WriteLine (m / n);
+        Console.WriteLine (m % n);
+        Console.WriteLine (m > n);
+        Console.WriteLine (m < n);
+        Console.WriteLine (m == n);
+        Console.WriteLine (m >= n);
+        Console.WriteLine (m <= n);
+        Console.WriteLine (m != n);
+
+        Console.WriteLine ("Operaciones y lógica variable Decimal");
+        decimal o = 1.35435634654654654654M;
+        decimal p = 2.36595684843813818723M;
+        Console.WriteLine (o + p);
+        Console.WriteLine (o - p);
+        Console.WriteLine (o * p);
+        Console.WriteLine (o / p);
+        Console.WriteLine (o % p);
+        Console.WriteLine (o > p);
+        Console.WriteLine (o < p);
+        Console.WriteLine (o == p);
+        Console.WriteLine (o >= p);
+        Console.WriteLine (o <= p);
+        Console.WriteLine (o != p);
+
+    }
+
+
+}
